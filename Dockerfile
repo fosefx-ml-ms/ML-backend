b42015f1567ee7f2b4b3d4a035f2b87d1409d40b
FoseFx <fosefx@pm.me>
2019-09-20 17:21:38 +0200

    Adds gRPC Server

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3-alpine as build
+RUN apk add --virtual .build-deps g++ python3-dev libffi-dev openssl-dev
+RUN pip3 install --upgrade pip setuptools
+RUN pip3 install grpcio-tools
+COPY protos protos
+RUN mkdir src
+RUN python -m grpc.tools.protoc -I=protos --python_out=src --grpc_python_out=src protos/ml.proto
+
+COPY src/backend.py src/backend.py
+CMD [ "python", "src/backend.py" ]
---
+++ src/backend.py
@@ -0,0 +1,27 @@
+from concurrent import futures
+import time
+import grpc
+import ml_pb2
+import ml_pb2_grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+class MLServicer(ml_pb2_grpc.MLBackendServicer):
+    def ClassifyImage(self, request, context):
+        print("Called")
+        print(self)
+        print(request)
+        print(context)
+
+
+if __name__ == '__main__':
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    ml_pb2_grpc.add_MLBackendServicer_to_server(MLServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("Server started")
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
