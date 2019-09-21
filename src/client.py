a54cbcb955313e26504a0c52e0a2ad270bb61891
FoseFx <fosefx@pm.me>
2019-09-21 14:13:42 +0200

    Removes need for hostname in cert

--- src/client.py
+++ src/client.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     with open('../secret/certificate.crt', 'rb') as cert_file:
         ssl_creds = grpc.ssl_channel_credentials(root_certificates=cert_file.read())
 
-    with grpc.secure_channel('localhost:50051', ssl_creds) as channel:
+    with grpc.secure_channel('localhost:50051', ssl_creds, options=(('grpc.ssl_target_name_override', 'hostname'),)) as channel:
         stub = ml_pb2_grpc.MLBackendStub(channel)
         with open("src/img.jpg", "rb") as image:
             f = image.read()
