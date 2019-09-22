c4173b0dcfd638b7fd113b54356bf4b58b19c23f
FoseFx <fosefx@pm.me>
2019-09-22 13:45:41 +0200

    Replaces hostname with localhost

--- generate_cert.sh
+++ generate_cert.sh
@@ -5,7 +5,7 @@ then
     then
         mkdir secret
     fi
-    echo "\n\n\n\n\nhostname\n\n" | openssl req -newkey rsa:2048 -nodes -keyout secret/key.pem -x509 -days 365 -out secret/certificate.crt
+    echo "\n\n\n\n\nlocalhost\n\n" | openssl req -newkey rsa:2048 -nodes -keyout secret/key.pem -x509 -days 365 -out secret/certificate.crt
     else
     echo "\n\tCert found, not re-generating"
 fi
